1e5e8f94d73bcf2746c413b01ea89b6b2814539c
Cawi <Cawi>
2016-09-11 22:46:59 +0800

    db setup initial commit

--- RugbyScoreApp/www/js/app.js
+++ RugbyScoreApp/www/js/app.js
@@ -3,22 +3,35 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform, $cordovaSQLite) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+
+
+      try {
+        db = $cordovaSQLite.openDB({ name: "rugbyapp.db", location: 'default' });
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS team (TeamId integer primary key, AbbrTeamName text, FullTeamName text, ClubAddress text, TownCity text, Country text, PostCode text)");
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS match (MatchId integer primary key, Team1 integer, Team2 integer, MatchDate text, MatchTime text, Location text, Team1Score text, Team2Score text)");
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS settings (SettingsId integer primary key, TeamId integer)");
+      }
+      catch (ex) {
+        alert(ex);
+      }
+
+
+    });
+  })
