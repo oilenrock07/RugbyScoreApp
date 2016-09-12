65e2f29af68c34146a887cb47eec6835c1ac9e3d
Cawi <Cawi>
2016-09-12 20:34:09 +0800

    Initial commit for New Match

--- RugbyScoreApp/www/js/controllers.js
+++ RugbyScoreApp/www/js/controllers.js
@@ -5,7 +5,24 @@ angular.module('rugbyapp.controllers', [])
 })
 
 .controller('MatchController', function ($scope){
+    $scope.matchId = 0;
+    $scope.team1 = '';
+    $scope.team2 = '';
+    $scope.location = '';
+    $scope.team1Score = 0;
+    $scope.team2Score = 0;
+    $scope.matchTime = '';
+    $scope.isMyTeam = false;
 
+    $scope.newMatchMyTeamClick = function() {
+        if ($scope.isMyTeam) {
+            //populate the team textbox
+            $scope.team1 = 'Test';
+        }
+        else {
+            $scope.team1 = '';
+        }
+    }
 })
 
 .controller('TeamController', function ($scope){
